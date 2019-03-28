0172c74ad37d4f3f8118cf7f9ce01252937d4120
github username <github email address>
2019-03-28 15:23:19 +0000

    team totals added

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -4,22 +4,27 @@ class Queen < ApplicationRecord
   has_many :queen_stats
 
 
- def self.get_queens_from_one_season
-   response_string = RestClient.get("http://www.nokeynoshade.party/api/seasons/11/queens")
-   response_hash = JSON.parse(response_string)
-   response_hash.each do |queen|
-   Queen.find_or_create_by(name: queen["name"], image_url: queen["image_url"], quote: queen["quote"])
- end
-end
-
- def total_score
-  queen_total = 0
-  self.queen_stats.each do |challenge|
-    if challenge
-      queen_total += challenge.win_mini_challenge + challenge.win_maxi_challenge + challenge.safe_or_survives + challenge.placed_in_top + challenge.placed_in_bottom + challenge.wins_lipsync + challenge.loses_lipsync + challenge.eliminated + challenge.takes_the_crown
+  def self.get_queens_from_one_season
+    response_string = RestClient.get("http://www.nokeynoshade.party/api/seasons/11/queens")
+    response_hash = JSON.parse(response_string)
+    response_hash.each do |queen|
+     Queen.find_or_create_by(name: queen["name"], image_url: queen["image_url"], quote: queen["quote"])
     end
   end
-  queen_total
- end
-  # total_score = win_maxi_challenge + win_mini_challenge + safe_or_survives + placed_in_top + placed_in_bottom + wins_lipsync + loses_lipsync + eliminated + takes_the_crown
+
+  def total_score
+    queen_total = 0
+    self.queen_stats.each do |challenge|
+      if challenge
+        queen_total += challenge.win_mini_challenge + challenge.win_maxi_challenge + challenge.safe_or_survives + challenge.placed_in_top + challenge.placed_in_bottom + challenge.wins_lipsync + challenge.loses_lipsync + challenge.eliminated + challenge.takes_the_crown
+      end
+    end
+    queen_total
+  end
+
+  def team_total
+    
+  end
+
+
 end
